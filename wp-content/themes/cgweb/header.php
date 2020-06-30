4b805f66201e6717c72bdc092e77de8fce49d865
xchgx <xchgx@163.com>
2020-06-30 22:43:15 +0800

    v2.0 修改首页

---
+++ wp-content/themes/cgweb/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+  <meta charset="utf-8">
+  <title>千锤百炼成钢 个人主页</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="千锤百炼成钢个人主页">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <base href="<?php echo get_template_directory_uri(); ?>/">
+  <meta property="og:title" content="千锤百炼成钢个人主页">
+  <meta property="og:type" content="blog">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
+  <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
+  <!--[if lt IE 9]>
+  <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+  <![endif]-->
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+  <?php wp_head(); ?>
+</head>
+
+<body>
--- wp-content/themes/cgweb/index.php
+++ wp-content/themes/cgweb/index.php
@@ -1,34 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <title>千锤百炼成钢</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="千锤百炼成钢个人主页">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <base href="<?php echo get_template_directory_uri(); ?>/">
-        <meta property="og:title" content="千锤百炼成钢个人主页">
-        <meta property="og:type" content="blog">
-        <meta property="og:url" content="">
-        <meta property="og:image" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
-        <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
-        <!--[if lt IE 9]>
-          <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
-        <![endif]-->
-        <link rel="manifest" href="site.webmanifest">
-        <link rel="apple-touch-icon" href="icon.png">
-        <!-- Place favicon.ico in the root directory -->
-
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-
-        <meta name="theme-color" content="#fafafa">
-    </head>
-
-    <body>
+<?php get_header(); ?>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-12">
@@ -45,14 +15,14 @@
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                 </button>
-                                <a class="navbar-brand" href="#">Brand</a>
+                                <a class="navbar-brand" href="#">千锤百炼</a>
                             </div>
 
                             <!-- Collect the nav links, forms, and other content for toggling -->
                             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                                 <ul class="nav navbar-nav">
-                                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                                    <li><a href="#">Link</a></li>
+                                    <li class="active"><a href="#">珍藏工具 <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="#">学习心得</a></li>
                                     <li class="dropdown">
                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                                         <ul class="dropdown-menu">
