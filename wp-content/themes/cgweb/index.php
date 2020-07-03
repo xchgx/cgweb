b8ed6c22562b39093bf22e9ff7b9454380a8e431
xchgx <xchgx@163.com>
2020-07-03 23:39:33 +0800

    v2.3 调整传送带指示灯位置

--- wp-content/themes/cgweb/index.php
+++ wp-content/themes/cgweb/index.php
@@ -38,43 +38,7 @@
               'walker' => new wp_bootstrap_navwalker()  //自定义walker
             ) );
             ?>
-          </div>
-          <!--            <ul class="nav navbar-nav">-->
-          <!--              <li class="active"><a href="#">珍藏工具 <span class="sr-only">(current)</span></a></li>-->
-          <!--              <li><a href="#">学习心得</a></li>-->
-          <!--              <li class="dropdown">-->
-          <!--                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
-          <!--                <ul class="dropdown-menu">-->
-          <!--                  <li><a href="#">Action</a></li>-->
-          <!--                  <li><a href="#">Another action</a></li>-->
-          <!--                  <li><a href="#">Something else here</a></li>-->
-          <!--                  <li role="separator" class="divider"></li>-->
-          <!--                  <li><a href="#">Separated link</a></li>-->
-          <!--                  <li role="separator" class="divider"></li>-->
-          <!--                  <li><a href="#">One more separated link</a></li>-->
-          <!--                </ul>-->
-          <!--              </li>-->
-          <!--            </ul>-->
-          <!--            <form class="navbar-form navbar-left">-->
-          <!--              <div class="form-group">-->
-          <!--                <input type="text" class="form-control" placeholder="Search">-->
-          <!--              </div>-->
-          <!--              <button type="submit" class="btn btn-default">Submit</button>-->
-          <!--            </form>-->
-          <!--            <ul class="nav navbar-nav navbar-right">-->
-          <!--              <li><a href="#">Link</a></li>-->
-          <!--              <li class="dropdown">-->
-          <!--                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
-          <!--                <ul class="dropdown-menu">-->
-          <!--                  <li><a href="#">Action</a></li>-->
-          <!--                  <li><a href="#">Another action</a></li>-->
-          <!--                  <li><a href="#">Something else here</a></li>-->
-          <!--                  <li role="separator" class="divider"></li>-->
-          <!--                  <li><a href="#">Separated link</a></li>-->
-          <!--                </ul>-->
-          <!--              </li>-->
-          <!--            </ul>-->
-          <!--          </div>/.navbar-collapse -->
+          </div><!--          /.navbar-collapse-->
         </div><!-- /.container-fluid -->
       </nav>
       <header role="header">
